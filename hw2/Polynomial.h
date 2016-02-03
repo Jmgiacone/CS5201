e188820d4cf9c72f3c4118f78f68c7475651e3d5
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-03 15:05:02 -0600

    Defined all overloaded operators

--- hw2/Polynomial.h
+++ hw2/Polynomial.h
@@ -1,9 +1,49 @@
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 template<class T>
+struct Term
+{
+  public:
+    T coefficient;
+    T exponent;
+};
+
+template <class T>
 class Polynomial
 {
+  private:
+    int numTerms;
+    Term<T>* data;
+  
+  public:
+    Polynomial(); 
+    Polynomial(const int n);
+    Polynomial(const Polynomial<T> &src); 
+    ~Polynomial();
+
+    //Overloaded Operators
+    Polynomial<T>& operator+=(const Polynomial<T>& rhs) const;
+    Polynomial<T>& operator+(const Polynomial<T>& rhs) const;
+    Polynomial<T>& operator-(const Polynomial<T>& rhs) const;
+    Polynomial<T>& operator-() const;
+    Polynomial<T>& operator-=(const Polynomial<T>& rhs) const;
+    bool operator==(const Polynomial<T>& rhs) const;
+    bool operator!=(const Polynomial<T>& rhs) const;
+    Polynomial<T>& operator*(const float f) const;
+    Polynomial<T>& operator=(const Polynomial<T>& rhs);
+    T& operator[](const int i) const;
+    float operator()(const int x) const;
+    Polynomial<T>& operator~() const;
+
+    template <class U> 
+    friend ostream& operator<<(ostream& out, const Polynomial<U> rhs);
 
 };
+
+
 #include "Polynomial.hpp"
 #endif
