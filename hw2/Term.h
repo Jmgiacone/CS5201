baad4e864e7692d5345c2f87739223a490a2394e
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-04 20:50:46 -0600

    Forgot to add semicolon

--- hw2/Term.h
+++ hw2/Term.h
@@ -15,7 +15,7 @@ struct Term
   T coefficient;
   T exponent;
   bool operator< (Term<T>& rhs) {return exponent < rhs.exponent;};
-  bool operator> (Term<T>& rhs) {return exponent > rhs.exponent};
+  bool operator> (Term<T>& rhs) {return exponent > rhs.exponent;};
   bool operator<= (Term<T>& rhs) {return exponent <= rhs.exponent;};
   bool operator>= (Term<T>& rhs) {return exponent >= rhs.exponent;};
   bool operator== (Term<T>& rhs) {return (coefficient == rhs.coefficient && exponent == rhs.exponent);};
