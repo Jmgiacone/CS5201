5bf70c0685ca5ea945d53cd2abc4287b36daff81
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-11 19:48:57 -0600

    Added operator() documentation

--- hw2/InfinityNorm.h
+++ hw2/InfinityNorm.h
@@ -4,6 +4,17 @@ template<class T>
 class InfinityNorm
 {
   public:
+    /**
+     * Purpose: To find the Infinity Norm given a vector of Polynomials
+                The Infinity Norm is defined as the maximum of the 
+                magnitudes of the given Polynomials
+     * Pre: Template type T must have the '=' operator defined
+            Template type T must have the '*' operator defined
+            Template type T must have the '+=' operator defined
+            Template type T must be compatible with function sqrt()
+     * Post: None
+     * Return: The Infinity Norm of the given vector
+     */
     const T operator() (const std::vector<Polynomial<T>>& vect) const;
 };
 #include "InfinityNorm.hpp"
