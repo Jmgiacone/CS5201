631167b1f32042cfa2ce506b24e3a7385d56779b
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-11 19:13:16 -0600

    Added operator documentation

--- hw2/Polynomial.h
+++ hw2/Polynomial.h
@@ -72,23 +72,171 @@ class Polynomial
     ~Polynomial();
 
     //Overloaded Operators
-    Polynomial<T>&operator= (const Polynomial<T>& rhs);
+    
+    /**
+     * Purpose: Takes rhs and copies all of the data in to this object
+     * Pre: Template type T must have the '=' operator defined
+     * Param: rhs - The object to be copied
+     * Post: All previous data will be deleted and replaced by data from rhs
+     * Return: The calling object so that '=' operators can be chained
+     */
+    Polynomial<T>& operator= (const Polynomial<T>& rhs);
+
+    /**
+     * Purpose: To check equality between the calling object and the parameter
+     * Pre: Template type T must have the == operator defined 
+     * Param: rhs - The object to be compared to the calling object
+     * Post: None
+     * Return: true if the objects are equal, false if they are not
+     */
     bool operator== (const Polynomial<T>& rhs) const;
+
+    /**
+     * Purpose: To check inequality between the calling object and the parameter
+     * Pre: Template type T must have the == operator defined
+     * Post: None
+     * Return: true if the objects are not equal, false if they are
+     */
     bool operator!= (const Polynomial<T>& rhs) const;
+
+    /**
+     * Purpose: To evaluate the function at a specified constant
+     * Pre: Template type T must have the '*' operator defined
+            Template type T must be compatible with function pow()
+            Template type T must have the '+=' operator defined
+     * Param: x - The value for the Polynomial to be evaluated at
+     * Post: None
+     * Return: The value of the Polynomial evaluated at x
+     */
     const T operator() (const T& x) const;
+
+    /**
+     * Purpose: To peek at the monomial in the specified position
+     * Pre: Parameter i must be a valid bound (between 0 and numTerms - 1)
+     * Param: i - The index of the monomial to be retrieved 
+     * Post: If parameter i is not a valid bound, a RangeError will be thrown
+     * Return: The monomial indexed at i
+     */
     const Term<T>& operator[] (const int i) const;
+
+    /**
+     * Purpose: To multiply the coefficients of the calling object by a constant
+     * Pre: Template type T must have the '*=' operator defined
+     * Param: x - The value to be multiplied across the coefficients
+     * Post: None
+     * Return: A copy of the calling object with all coefficients 
+               multiplied by x 
+     */
     const Polynomial<T> operator* (const T& x) const;
+
+    /**
+     * Purpose: To add two Polynomials together
+     * Pre: Template type T must have the '<' operator defined
+            Template type T must have the '>' operator defined
+            Template type T must have the '=' operator defined
+            Template type T must have the '+' operator defined
+            Template type T must have the '==' operator defined
+     * Param: rhs - The Polynomial to be added to the calling object
+     * Post: None
+     * Return: A Polynomial representing the sum of the two Polynomials
+     */
     const Polynomial<T> operator+ (const Polynomial<T>& rhs) const;
+
+    /**
+     * Purpose: To add two Polynomials together and set the calling object
+                equal to the result
+     * Pre: Template type T must have the '<' operator defined
+            Template type T must have the '>' operator defined
+            Template type T must have the '=' operator defined 
+            Template type T must have the '+' operator defined
+            Template type T must have the '==' operator defined
+     * Post: The calling object will be modified to be the sum of itself
+             and the parameter rhs
+     * Return: The calling object so that '+=' operators can be 
+               chained together
+     */
     const Polynomial<T>& operator+= (const Polynomial<T>& rhs);
+    
+    /**
+     * Purpose: To subtract the parameter from the calling object
+     * Pre: Template type T must have the '<' operator defined
+            Template type T must have the '>' operator defined
+            Template type T must have the '=' operator defined
+            Template type T must have the '+' operator defined
+            Template type T must have the '*' operator defined
+            Template type T must have the '==' operator defined
+     * Param: rhs - The Polynomial to be subtracted from the calling object
+     * Post: None
+     * Return: A Polynomial representing the difference of the two Polynomials
+     */
     const Polynomial<T> operator- (const Polynomial<T>& rhs) const;
+
+    /**
+     * Purpose: To subtract the parameter from the calling object
+                and assign that difference to the calling object
+     * Pre: Template type T must have the '<' operator defined
+            Template type T must have the '>' operator defined
+            Template type T must have the '=' operator defined
+            Template type T must have the '+' operator defined
+            Template type T must have the '==' operator defined
+     * Param: rhs - The Polynomial to be subtracted from the calling object
+     * Post: The calling object will be modified to the difference between 
+             the calling itself and the parameter rhs
+     * Return: The calling object so that '-=' operators can be
+               chained together
+     */
     const Polynomial<T>& operator-= (const Polynomial<T>& rhs);
+    
+    /**
+     * Purpose: To make the coefficient on each term of the 
+                Polynomial negative
+     * Pre: Template type T must have the '>' operator defined 
+            Template type T must have the '*=' operator defined
+     * Post: None
+     * Return: A copy of the calling object where every coefficient
+               is made to be negative
+     */
     const Polynomial<T> operator~ () const;
+    
+    /**
+     * Purpose: To negate the coefficient on each tearm of the Polynomial 
+     * Pre: Template type T must have the '*=' operator defined
+     * Post: None
+     * Return: A copy of the calling object where every coefficient
+               is negated
+     */
     const Polynomial<T> operator- () const;
 
-    //Magnitude function
+    /**
+     * Purpose: To calculate and return the magnitude of the calling object.
+                The magnitude is defined as the square root of the sum of
+                the sum of the squares of the coefficients
+     * Pre: Template type T must have the '*' operator defined
+            Template type T must have the '+=' operator defined
+            Template type T must be compatible with the sqrt() function
+     * Post: None
+     * Return: The magnitude of the calling object
+     */
     const T magnitude() const;
 
+    /**
+     * Purpose: To multiply the coefficients of the calling rhs by a constant
+     * Pre: Template type T must have the '*=' operator defined 
+     * Param: x - The value to be multiplied across the coefficients
+              rhs - The Polynomial object to supply the coefficients
+     * Post: None
+     * Return: A copy of rhs with all coefficients multiplied by x 
+     */
     friend Polynomial<T> operator* (const T& x, const Polynomial<T>& rhs) {return rhs * x;};
+
+    /**
+     * Purpose: To output the given Polynomial using the provided ostream
+     * Pre:
+     * Param: out - The ostream to be used for output
+              rhs - The Polynomial to be outputted to the given ostream
+     * Post: out will have characters inserted in to it's stream
+       Return: The ostream parameter rhs so that the '<<' operator can be chained
+     */
     friend std::ostream& operator<< <>(std::ostream& out, const Polynomial<T>& rhs);
 };
 #include "Polynomial.hpp"
