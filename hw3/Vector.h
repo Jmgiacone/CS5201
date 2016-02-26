af2fa781d5a7e88b28808d75fc0b71613d74a41a
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-25 23:53:32 -0600

    Wrote documentation for constructors and member functions

--- hw3/Vector.h
+++ hw3/Vector.h
@@ -49,23 +49,117 @@ class Vector
 
   public:
     //Conventional Constructors
+    /**
+     * Purpose: Default constructor
+     * Pre: None
+     * Param: None
+     * Post: The object is constructed
+     * Return: None
+     */
     Vector();
+
+    /**
+     * Purpose: Parameterized constructor
+     * Pre: s must be non-zero
+     * Param: s - The size of the Vector to be constructed
+     * Post: The object is constructed
+     *       If s is not non-zero, an invalid_argument exception will bet thrown
+     * Return: None
+     */
     Vector(int s);
+
+    /**
+     * Purpose: Copy constructor
+     * Pre: None
+     * Param: src - The object to be copied from
+     * Post: The calling object will be a deep copy of the parameter src
+     * Return: None
+     */
     Vector(const Vector<T>& src);
+
+    /**
+     * Purpose: Destructor
+     * Pre: None
+     * Param: None
+     * Post: The object is destructed
+     * Return: None
+     */
     ~Vector();
 
-    //Move Constructor
+    /**
+     * Purpose: Move constructor
+     * Pre: None
+     * Param: src - A temporary 'rvalue' that is about to be destructed
+     * Post: The calling object 'steals' the data pointer from src and sets src's data to NULL
+     *       The calling object will be a deep copy of the parameter src
+     * Return: None
+     */
     Vector(Vector<T>&& src) : data(src.data), terms(src.terms)
     {
       src.data = NULL;
     };
 
+    /**
+     * Purpose: To return the number of terms in the Vector
+     * Pre: None
+     * Param: None
+     * Post: None
+     * Return: The number of terms in the Vector
+     */
     const int numTerms() const {return terms;};
-    //Conventional Operators
+
+    /**
+     * Purpose: Assignment Operator - To assign the calling object to the state of the right-hand-side object
+     * Pre: None
+     * Param: rhs - The object to be copied from
+     * Post: The calling object will be in the same state as parameter rhs
+     * Return: The calling object for the purpose of chaining = operators
+     */
     const Vector<T>& operator= (Vector<T> rhs);
+
+    /**
+     * Purpose: += Operator - To add the calling object and the parameter rhs together, then set the calling object
+     *             equal to the result
+     * Pre: The calling object and parameter rhs must have the same number of terms
+     *      Template type T must have operator= (T) defined
+     *      Template type T must have operator+ (T, T) defined
+     * Param: rhs - The Vector to be added
+     * Post: If the calling object and rhs have differing numbers of terms, a std::length_error will be thrown
+     *       The calling object will be the sum of the calling object and the parameter rhs
+     * Return: The calling object for the purpose of chaining += operators
+     */
     const Vector<T>& operator+= (const Vector<T>& rhs);
+
+    /**
+     * Purpose: += Operator - To subtract the parameter rhs from the calling object, then set the calling object
+     *             equal to the result
+     * Pre: The calling object and parameter rhs must have the same number of terms
+     *      Template type T must have operator= (T) defined
+     *      Template type T must have operator+ (T, T) defined
+     *      Template type T must have operator- (T) defined <--- This is unary - or the negation operator
+     * Param: rhs - The Vector to be subtracted
+     * Post: If the calling object and rhs have differing numbers of terms, a std::length_error will be thrown
+     *       The calling object will be the difference of the calling object and the parameter rhs
+     * Return: The calling object for the purpose of chaining -= operators
+     */
     const Vector<T>& operator-= (const Vector<T>& rhs);
+
+    /**
+     * Purpose: [] operator - To be able to peek at and modify elements of the Vector
+     * Pre: Parameter x must be within the range [0, terms)
+     * Param: x - The index of the Vector element to be returned
+     * Post: If parameter x is not withing the proper range, a out_of_range exception will be thrown
+     * Return: The element indexed at parameter x
+     */
     T& operator[] (const int x);
+
+    /**
+     * Purpose: [] operator - To be able to peek at and NOT modify elements of the Vector
+     * Pre: Parameter x must be within the range [0, terms)
+     * Param: x - The index of the Vector element to be returned
+     * Post: If parameter x is not withing the proper range, a out_of_range exception will be thrown
+     * Return: The element indexed at parameter x
+     */
     const T& operator[] (const int x) const;
 
     friend std::ostream& operator<< <>(std::ostream& out, const Vector<T>& rhs);
