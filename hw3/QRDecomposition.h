7749cd506a7c5aac16fd366fffaeddc71ff55657
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-26 00:33:05 -0600

    Add operator() documentation

--- hw3/QRDecomposition.h
+++ hw3/QRDecomposition.h
@@ -9,6 +9,15 @@ template <class T>
 class QRDecomposition
 {
   public:
+    /**
+     * Purpose: operator() - Function evaluation operator
+     * Pre: Template type T must have operator== (T, int) defined
+     *      Template type T must have operator/ (int, T) defined
+     *      Template type T must have operator= (int) defined
+     * Param: a - The matrix to be decomposed
+     * Post: If there is a division by zero, a domain_error exception is thrown
+     * Return: A standard vector of two matrices, ordered Q and R
+     */
     std::vector<std::vector<Vector<T>>> operator() (const std::vector<Vector<T>>& a);
 };
 
