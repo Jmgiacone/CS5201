d292584c3b95e08bd299a0d250b2d867c1cce0a3
Jordan Giacone <Jmgiacone@gmail.com>
2016-02-29 21:27:41 -0600

    Got Matrix class and function declarations laid out

---
+++ hw4/Matrix.h
@@ -0,0 +1,91 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <iostream>
+#include "Vector.h"
+
+using std::ostream
+using std::istream
+template<class T>
+class Matrix;
+
+template <class T>
+ostream& operator<< (ostream& out, const Matrix<T>& rhs);
+
+template <class T>
+istream& operator>> (istream& in, Matrix<T>& rhs);
+
+template <class T>
+Matrix<T> operator+ (const Matrix<T>& lhs, const Matrix<T>& rhs);
+
+template <class T>
+Matrix<T> operator- (const Matrix<T>& lhs, const Matrix<T>& rhs);
+
+template <class T>
+Matrix<T> operator- (const Matrix<T>& rhs);
+
+template <class T>
+Matrix<T> operator* (const Matrix<T>& lhs, const Matrix<T>& rhs);
+
+template <class T>
+Vector<T> operator* (const Matrix<T>& lhs, const Vector<T>& rhs);
+
+template <class T>
+Vector<T> operator* (const Vector<T>& lhs, const Matrix<T>& rhs);
+
+template <class T>
+T operator* (const T& lhs, const Matrix<T>& rhs);
+
+template <class T>
+T operator* (const Matrix<T>& lhs, const T& rhs);
+
+template <class T>
+bool operator== (const Matrix<T>& lhs, const Matrix<T>& rhs);
+
+template <class T>
+bool operator!= (const Matrix<T>& lhs, const Matrix<T>& rhs);
+
+template <class T>
+void swap (Matrix<T>& x, Matrix<T>& y);
+
+template <class T>
+class Matrix
+{
+  private:
+    int rows;
+    int columns;
+    Vector<T>* data;
+
+  public:
+    Matrix ();
+    Matrix (int r, int c);
+    Matrix (const Matrix<T>& src);
+    Matrix (Matrix<T>&& src);
+    ~Matrix ();
+    const T& operator[] (int x) const;
+    T& operator[] (int x);
+    Matrix<T>& operator= (Matrix<T> rhs);
+    Matrix<T>& operator= (Matrix<T>&& rhs);
+    Matrix<T>& operator+= (const Matrix<T>& rhs);
+    Matrix<T>& operator+= (Matrix<T>&& rhs);
+    Matrix<T>& operator-= (const Matrix<T>& rhs);
+    Matrix<T>& operator-= (Matrix<T>&& rhs);
+    int getRows () const;
+    int getColumns () const;
+    
+    //Friends
+    friend ostream& operator<< <>(ostream& out, const Matrix<T>& rhs);
+    friend istream& operator>> <>(istream& in, Matrix<T>& rhs);
+    friend Matrix<T> operator+ <>(const Matrix<T>& lhs, const Matrix<T>& rhs);
+    friend Matrix<T> operator- <>(const Matrix<T>& lhs, const Matrix<T>& rhs);
+    friend Matrix<T> operator- <>(const Matrix<T>& rhs);
+    friend Matrix<T> operator* <>(const Matrix<T>& lhs, const Matrix<T>& rhs);
+    friend Vector<T> operator* <>(const Matrix<T>& lhs, const Vector<T>& rhs);
+    friend Vector<T> operator* <>(const Vector<T>& lhs, const Matrix<T>& rhs);
+    friend T operator* <>(const T& lhs, const Matrix<T>& rhs);
+    friend T operator* <>(const Matrix<T>& lhs, const T& rhs);
+    friend bool operator== <>(const Matrix<T>& lhs, const Matrix<T>& rhs);
+    friend bool operator!= <>(const Matrix<T>& lhs, const Matrix<T>& rhs);
+    friend void swap <>(Matrix<T>& x, Matrix<T>& y);
+};
+#include "Matrix.hpp"
+#endif
