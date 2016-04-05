dd4a8226a9f7a3a641ee7df7d86e4675f71a4b1c
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-04 22:28:18 -0500

    Initial skeleton of Matrix interface

---
+++ hw5/Matrix.h
@@ -0,0 +1,24 @@
+/**
+ * Programmer: Jordan Giacone
+ * Class: CS5201 Section A
+ * Instructor: Clayton Price
+ * Homework 5
+ * Student ID: 12400927
+ * Login: Jmgv27
+ * Date: 4/8/16
+ * Filename: Matrix.h
+ * Description: This is an Interface Base for an abstract Matrix
+ */
+
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+
+class Matrix
+{
+
+};
+
+
+#endif
