dc82791141a3fcd5b3a5343d4d1155156652d498
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-05 00:34:40 -0500

    Added in all friends

--- hw5/Matrix.hpp
+++ hw5/Matrix.hpp
@@ -21,3 +21,63 @@ Matrix<T>& Matrix<T>::operator-= (const Matrix<T>& rhs)
 {
   return (*this = operatorMinus(*this, rhs));
 }
+
+template <class T>
+ostream& operator<< (ostream& out, const Matrix<T>& rhs)
+{
+  return operatorOutput(out, rhs);
+}
+
+template <class T>
+Matrix<T> operator+ (const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+  return operatorPlus(lhs, rhs);
+}
+
+template <class T>
+Matrix<T> operator- (const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+  return operatorMinus(lhs, rhs);
+}
+
+template <class T>
+Matrix<T> operator* (const T& lhs, const Matrix<T>& rhs)
+{
+  return operatorAsterisk(lhs, rhs);
+}
+
+template <class T>
+Matrix<T> operator* (const Matrix<T>& lhs, const T& rhs)
+{
+  return operatorAsterisk(lhs, rhs);
+}
+
+template <class T>
+Matrix<T> operator* (const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+  return operatorAsterisk(lhs, rhs);
+}
+
+template <class T>
+Vector<T> operator* (const Matrix<T>& lhs, const Vector<T>& rhs)
+{
+  return operatorAsterisk(lhs, rhs);
+}
+
+template <class T>
+Vector<T> operator* (const Vector<T>& lhs, const Matrix<T>& rhs)
+{
+  return operatorAsterisk(lhs, rhs);
+}
+
+template <class T>
+bool operator== (const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+  return operatorDoubleEqual(lhs, rhs);
+}
+
+template <class T>
+bool operator!= (const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+  return operatorBangEqual(lhs, rhs);
+}
