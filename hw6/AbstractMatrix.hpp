03ecfb10799676d56107d576d90623d2c0dabebd
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-16 16:42:14 -0500

    Implemented operator*

--- hw6/AbstractMatrix.hpp
+++ hw6/AbstractMatrix.hpp
@@ -3,3 +3,9 @@ ostream& operator<< (ostream& out, const AbstractMatrix<T>& rhs)
 {
   return rhs.output(out);
 }
+
+template <class T>
+Vector<T> operator* (const AbstractMatrix<T>& lhs, const Vector<T>& rhs)
+{
+  return lhs.vectorMultiplication(rhs);
+}
