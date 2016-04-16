56509ca39d39df4bf6b5eecfc7043ee5fca55737
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-16 17:00:44 -0500

    Implemented destructor

--- hw6/SymmetricDenseMatrix.hpp
+++ hw6/SymmetricDenseMatrix.hpp
@@ -22,3 +22,12 @@ SymmetricDenseMatrix<T>::SymmetricDenseMatrix(const SymmetricDenseMatrix<T>& src
     data[i] = src.data[i];
   }
 }
+
+template <class T>
+SymmetricDenseMatrix<T>::~SymmetricDenseMatrix()
+{
+  if(data != NULL)
+  {
+    delete [] data;
+  }
+}
