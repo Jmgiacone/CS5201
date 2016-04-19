4b02b8aa9f23e60c7d616ca5502b261315ccb08b
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-18 22:36:28 -0500

    Finished Preliminary

--- hw6/DiagonalMatrix.h
+++ hw6/DiagonalMatrix.h
@@ -1,17 +1,49 @@
-//
-// Created by jordan on 4/9/16.
-//
-
 #ifndef DIAGONALMATRIX_H
 #define DIAGONALMATRIX_H
-
 #include "AbstractMatrix.h"
+
+template <class T>
+class DiagonalMatrix;
+
+template <class T>
+DiagonalMatrix<T> operator+ (const DiagonalMatrix<T>& lhs, const DiagonalMatrix<T>& rhs);
+
+template <class T>
+DiagonalMatrix<T> operator- (const DiagonalMatrix<T>& rhs);
+
+template <class T>
+DiagonalMatrix<T> operator- (const DiagonalMatrix<T>& lhs, const DiagonalMatrix<T>& rhs);
 
 template <class T>
 class DiagonalMatrix : public AbstractMatrix<T>
 {
+  private:
+    int dimensions;
+    Vector<T>* data;
+    static T zero = 0;
+  public:
+    DiagonalMatrix() : DiagonalMatrix(DEFAULT_DIMENSIONS) {};
+    DiagonalMatrix(int d);
+    DiagonalMatrix(const DiagonalMatrix<T>& src);
+    DiagonalMatrix(DiagonalMatrix<T>&& src) : dimensions(src.dimensions), data(src.data) {src.data = NULL;}
+    ~DiagonalMatrix();
 
+    virtual int numRows() const {return dimensions;}
+    virtual int numColumns() const {return dimensions;}
+    virtual AbstractMatrix<T>& operator+= (const AbstractMatrix<T>& rhs);
+    virtual AbstractMatrix<T>& operator-= (const AbstractMatrix<T>& rhs);
+    virtual T& operator()(const int row, const int column);
+    virtual T& operator()(const int row, const int column) const;
+    virtual AbstractMatrix<T>* clone() const;
+    virtual Vector<T> gaussianElimination(Vector<T> bVector) const;
+
+    //Friends
+    friend DiagonalMatrix<T> operator+ <>(const DiagonalMatrix<T>& lhs, const DiagonalMatrix<T>& rhs);
+    friend DiagonalMatrix<T> operator- <>(const DiagonalMatrix<T>& rhs);
+    friend DiagonalMatrix<T> operator- <>(const DiagonalMatrix<T>& lhs, const DiagonalMatrix<T>& rhs);
+protected:
+    virtual Vector<T> vectorMultiplication(const Vector<T>& bVector) const;
+    virtual ostream& output(ostream& out) const;
 };
-
 #include "DiagonalMatrix.hpp"
 #endif
--- hw6/DiagonalMatrix.hpp
+++ hw6/DiagonalMatrix.hpp
@@ -1,5 +1,164 @@
 //
 // Created by jordan on 4/9/16.
 //
+template <class T>
+DiagonalMatrix<T>::DiagonalMatrix(int d)
+{
+  dimensions = d;
 
-#include "DiagonalMatrix.h"
+  data = new Vector<T>[dimensions];
+}
+
+template <class T>
+DiagonalMatrix<T>::DiagonalMatrix(const DiagonalMatrix<T>& src)
+{
+  dimensions = src.dimensions;
+  data = new Vector<T>[dimensions];
+
+  for(int i = 0; i < dimensions; i++)
+  {
+    data[i] = src.data[i];
+  }
+}
+
+template <class T>
+DiagonalMatrix<T>::~DiagonalMatrix()
+{
+  if(data != NULL)
+  {
+    delete [] data;
+    data = NULL;
+  }
+}
+
+template <class T>
+AbstractMatrix<T>& DiagonalMatrix<T>::operator+=(const AbstractMatrix<T> &rhs)
+{
+  return *this = *this + dynamic_cast<const DiagonalMatrix<T>&>(rhs);
+}
+
+template <class T>
+AbstractMatrix<T>& DiagonalMatrix<T>::operator-=(const AbstractMatrix<T> &rhs)
+{
+  return *this = *this - dynamic_cast<const DiagonalMatrix<T>&>(rhs);
+}
+
+template <class T>
+T& DiagonalMatrix<T>::operator()(const int row, const int column)
+{
+  zero = 0;
+  if(row < 0 || row >= dimensions || column < 0 || column >= dimensions)
+  {
+    throw std::invalid_argument("DiagonalMatrix::(): Invalid row/column");
+  }
+
+  if(row == column)
+  {
+    return data[row];
+  }
+
+  return zero;
+}
+
+template <class T>
+T& DiagonalMatrix<T>::operator()(const int row, const int column) const
+{
+  zero = 0;
+  if(row < 0 || row >= dimensions || column < 0 || column >= dimensions)
+  {
+    throw std::invalid_argument("DiagonalMatrix::(): Invalid row/column");
+  }
+
+  if(row == column)
+  {
+    return data[row];
+  }
+
+  return zero;
+}
+
+template <class T>
+AbstractMatrix<T>* DiagonalMatrix<T>::clone() const
+{
+  return new DiagonalMatrix<T>(*this);
+}
+
+template <class T>
+DiagonalMatrix<T> operator+ (const DiagonalMatrix<T>& lhs, const DiagonalMatrix<T>& rhs)
+{
+  if(lhs.dimensions != rhs.dimensions)
+  {
+    throw std::invalid_argument("DiagonalMatrix::+: Matrices are of different sizes");
+  }
+
+  DiagonalMatrix<T> sum(lhs.dimensions);
+
+  for(int i = 0; i < rhs.dimensions; i++)
+  {
+    sum.data[i] = lhs.data[i] + rhs.data[i];
+  }
+
+  return sum;
+}
+
+template <class T>
+DiagonalMatrix<T> operator- (const DiagonalMatrix<T>& lhs, const DiagonalMatrix<T>& rhs)
+{
+  return lhs + -rhs;
+}
+
+template <class T>
+DiagonalMatrix<T> operator- (const DiagonalMatrix<T>& rhs)
+{
+  DiagonalMatrix<T> negation(rhs.dimensions);
+
+  for(int i = 0; i < rhs.dimensions; i++)
+  {
+    negation.data[i] = -rhs.data[i];
+  }
+
+  return negation;
+}
+
+template <class T>
+ostream& DiagonalMatrix<T>::output(ostream &out) const
+{
+  for(int i = 0; i < dimensions; i++)
+  {
+    for(int j = 0; j < dimensions; j++)
+    {
+      if(j != 0)
+      {
+        out << std::setw(10);
+      }
+      else if ((*this)(i, j) >= 0)
+      {
+        out << " ";
+      }
+      out << std::fixed << std::setprecision(5) << (*this)(i, j) << "\t";
+    }
+
+    if (i == dimensions - 1)
+    {
+
+    }
+    else
+    {
+      out << std::endl;
+    }
+  }
+
+  return out;
+}
+
+template <class T>
+Vector<T> DiagonalMatrix<T>::gaussianElimination(Vector<T> bVector) const
+{
+  return bVector;
+}
+
+template <class T>
+Vector<T> DiagonalMatrix<T>::vectorMultiplication(const Vector<T> &bVector) const
+{
+  return bVector;
+}
