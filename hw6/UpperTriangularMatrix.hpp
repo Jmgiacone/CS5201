0672cad57ec31ea7fa981fcf3092d6855bf06507
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-19 20:21:39 -0500

    Implemented constructor/destructor

--- hw6/UpperTriangularMatrix.hpp
+++ hw6/UpperTriangularMatrix.hpp
@@ -1,5 +1,34 @@
-//
-// Created by jordan on 4/9/16.
-//
+template <class T>
+UpperTriangularMatrix<T>::UpperTriangularMatrix(const int d)
+{
+  dimensions = d;
+  data = new Vector<T>[dimensions];
 
-#include "UpperTriangularMatrix.h"
+  for(int i = 0; i < dimensions; i++)
+  {
+    data[i] = Vector<T>(dimensions - i);
+  }
+}
+
+template <class T>
+UpperTriangularMatrix<T>::UpperTriangularMatrix(const UpperTriangularMatrix<T>& src)
+{
+  dimensions = src.dimensions;
+  data = new Vector<T>[dimensions];
+
+  for(int i = 0; i < dimensions; i++)
+  {
+    data[i] = src.data[i];
+  }
+}
+
+template <class T>
+UpperTriangularMatrix::~UpperTriangularMatrix()
+{
+  if (data != NULL)
+  {
+    delete[] data;
+    data = NULL;
+  }
+}
+
