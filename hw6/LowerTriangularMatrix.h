d7043103e36dbae0a82b1cad5ca62f914df0ea92
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-19 21:57:47 -0500

    Carbon copy of upper with one thing changed

--- hw6/LowerTriangularMatrix.h
+++ hw6/LowerTriangularMatrix.h
@@ -6,11 +6,51 @@
 #define LOWERTRIANGULARMATRIX_H
 
 #include "AbstractMatrix.h"
+template <class T>
+class LowerTriangularMatrix;
+
+template <class T>
+LowerTriangularMatrix<T> operator+ (const LowerTriangularMatrix<T>& lhs, const LowerTriangularMatrix<T>& rhs);
+
+template <class T>
+LowerTriangularMatrix<T> operator- (const LowerTriangularMatrix<T>& rhs);
+
+template <class T>
+LowerTriangularMatrix<T> operator- (const LowerTriangularMatrix<T>& lhs, const LowerTriangularMatrix<T>& rhs);
+
+template <class T>
+void swap(LowerTriangularMatrix<T>& x, LowerTriangularMatrix<T>& y);
 
 template <class T>
 class LowerTriangularMatrix : public AbstractMatrix<T>
 {
+  private:
+    static T zero = 0;
+    int dimensions;
+    Vector<T>* data;
 
+  public:
+    LowerTriangularMatrix() : LowerTriangularMatrix(DEFAULT_TERMS) {}
+    LowerTriangularMatrix(const int d);
+    LowerTriangularMatrix(const LowerTriangularMatrix<T>& src);
+    LowerTriangularMatrix(LowerTriangularMatrix<T>&& src) : dimensions(src.dimensions), data(src.data) {src.data = NULL;}
+    ~LowerTriangularMatrix();
+
+    LowerTriangularMatrix<T>& operator= (LowerTriangularMatrix<T> rhs);
+    LowerTriangularMatrix<T>& operator+= (const LowerTriangularMatrix<T>& rhs);
+    LowerTriangularMatrix<T>& operator-= (const LowerTriangularMatrix<T>& rhs);
+
+    virtual T& operator()(const int row, const int column) const;
+    virtual T& operator()(const int row, const int column);
+    virtual AbstractMatrix<T>* clone() const;
+    virtual Vector<T> gaussElimination(Vector<T> bVector) const;
+    virtual Vector<T> vectorMultiplication(const Vector<T>& bVector) const;
+
+    //Friends
+    friend LowerTriangularMatrix<T> operator+ <>(const LowerTriangularMatrix<T>& lhs, const LowerTriangularMatrix<T>& rhs);
+    friend LowerTriangularMatrix<T> operator- <>(const LowerTriangularMatrix<T>& rhs);
+    friend LowerTriangularMatrix<T> operator- <>(const LowerTriangularMatrix<T>& lhs, const LowerTriangularMatrix<T>& rhs);
+    friend void swap <>(LowerTriangularMatrix<T>& x, LowerTriangularMatrix<T>& y);
 };
 #include "LowerTriangularMatrix.hpp"
 #endif
--- hw6/LowerTriangularMatrix.hpp
+++ hw6/LowerTriangularMatrix.hpp
@@ -1,5 +1,110 @@
-//
-// Created by jordan on 4/9/16.
-//
+template <class T>
+LowerTriangularMatrix<T>::LowerTriangularMatrix(const int d)
+{
+  dimensions = d;
+  data = new Vector<T>[dimensions];
 
-#include "LowerTriangularMatrix.h"
+  for(int i = 0; i < dimensions; i++)
+  {
+    data[i] = Vector<T>(dimensions - i);
+  }
+}
+
+template <class T>
+LowerTriangularMatrix<T>::LowerTriangularMatrix(const LowerTriangularMatrix<T>& src)
+{
+  dimensions = src.dimensions;
+  data = new Vector<T>[dimensions];
+
+  for(int i = 0; i < dimensions; i++)
+  {
+    data[i] = src.data[i];
+  }
+}
+
+template <class T>
+LowerTriangularMatrix<T>::~LowerTriangularMatrix()
+{
+  if (data != NULL)
+  {
+    delete[] data;
+    data = NULL;
+  }
+}
+
+template <class T>
+LowerTriangularMatrix<T>& LowerTriangularMatrix<T>::operator=(LowerTriangularMatrix<T> rhs)
+{
+  swap(*this, rhs);
+
+  return *this;
+}
+
+template <class T>
+LowerTriangularMatrix<T>& LowerTriangularMatrix<T>::operator+=(const LowerTriangularMatrix<T> &rhs)
+{
+  return *this = *this + rhs;
+}
+
+template <class T>
+LowerTriangularMatrix<T>& LowerTriangularMatrix<T>::operator-=(const LowerTriangularMatrix<T> &rhs)
+{
+  return *this = *this - rhs;
+}
+
+template <class T>
+void swap(LowerTriangularMatrix<T> &x, LowerTriangularMatrix<T> &y)
+{
+  std::swap(x.dimensions, y.dimensions);
+  std::swap(x.data, y.data);
+}
+
+template <class T>
+T& LowerTriangularMatrix<T>::operator()(const int row, const int column) const
+{
+  if (zero != 0)
+  {
+    zero = 0;
+  }
+
+  if(row >= column)
+  {
+    return data[column - row][row];
+  }
+
+  return zero;
+}
+
+template <class T>
+T& LowerTriangularMatrix<T>::operator()(const int row, const int column)
+{
+  if (zero != 0)
+  {
+    zero = 0;
+  }
+
+  if(row >= column)
+  {
+    return data[column - row][row];
+  }
+
+  return zero;
+}
+
+template <class T>
+AbstractMatrix<T>* LowerTriangularMatrix<T>::clone() const
+{
+  return new LowerTriangularMatrix<T>(*this);
+}
+
+template <class T>
+Vector<T> LowerTriangularMatrix<T>::gaussElimination(Vector<T> bVector) const
+{
+  return bVector;
+}
+
+template <class T>
+Vector<T> LowerTriangularMatrix<T>::vectorMultiplication(const Vector<T> &bVector) const
+{
+  return bVector;
+}
