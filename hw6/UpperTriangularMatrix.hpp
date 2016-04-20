329c52cf0b3c787f9530b6f8dff48db7867ab628
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-19 21:57:31 -0500

    Implementation

--- hw6/UpperTriangularMatrix.hpp
+++ hw6/UpperTriangularMatrix.hpp
@@ -23,7 +23,7 @@ UpperTriangularMatrix<T>::UpperTriangularMatrix(const UpperTriangularMatrix<T>& 
 }
 
 template <class T>
-UpperTriangularMatrix::~UpperTriangularMatrix()
+UpperTriangularMatrix<T>::~UpperTriangularMatrix()
 {
   if (data != NULL)
   {
@@ -32,3 +32,79 @@ UpperTriangularMatrix::~UpperTriangularMatrix()
   }
 }
 
+template <class T>
+UpperTriangularMatrix<T>& UpperTriangularMatrix<T>::operator=(UpperTriangularMatrix<T> rhs)
+{
+  swap(*this, rhs);
+
+  return *this;
+}
+
+template <class T>
+UpperTriangularMatrix<T>& UpperTriangularMatrix<T>::operator+=(const UpperTriangularMatrix<T> &rhs)
+{
+  return *this = *this + rhs;
+}
+
+template <class T>
+UpperTriangularMatrix<T>& UpperTriangularMatrix<T>::operator-=(const UpperTriangularMatrix<T> &rhs)
+{
+  return *this = *this - rhs;
+}
+
+template <class T>
+void swap(UpperTriangularMatrix<T> &x, UpperTriangularMatrix<T> &y)
+{
+  std::swap(x.dimensions, y.dimensions);
+  std::swap(x.data, y.data);
+}
+
+template <class T>
+T& UpperTriangularMatrix<T>::operator()(const int row, const int column) const
+{
+  if (zero != 0)
+  {
+    zero = 0;
+  }
+
+  if(row <= column)
+  {
+    return data[column - row][row];
+  }
+
+  return zero;
+}
+
+template <class T>
+T& UpperTriangularMatrix<T>::operator()(const int row, const int column)
+{
+  if (zero != 0)
+  {
+    zero = 0;
+  }
+
+  if(row <= column)
+  {
+    return data[column - row][row];
+  }
+
+  return zero;
+}
+
+template <class T>
+AbstractMatrix<T>* UpperTriangularMatrix<T>::clone() const
+{
+  return new UpperTriangularMatrix<T>(*this);
+}
+
+template <class T>
+Vector<T> UpperTriangularMatrix<T>::gaussElimination(Vector<T> bVector) const
+{
+  return bVector;
+}
+
+template <class T>
+Vector<T> UpperTriangularMatrix<T>::vectorMultiplication(const Vector<T> &bVector) const
+{
+  return bVector;
+}
