d6dca67067108888778e99c81292409e2f87e52f
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-22 10:58:36 -0500

    Documentation

--- hw6/QRDecomposition.h
+++ hw6/QRDecomposition.h
@@ -18,9 +18,41 @@ template <class T>
 class QRDecomposition
 {
   public:
+    /**
+     * Purpose: Default Constructor
+     * Pre: None
+     * Param: None
+     * Post: The object is initialized to a DenseMatrix of DEFAULT_ROWS x DEFAULT_COLUMNS
+     * Return: None
+     */
     QRDecomposition();
+
+    /*
+     * Purpose: Operator() - Do the decomposition
+     * Pre: None
+     * Param: aMatrix - The Matrix to be QR decomposed
+     * Post: If there is a division by zero, a domain_error exception will be thrown
+     *       q and r are set equal to the Q and R respectively
+     * Return: None
+     */
     void operator()(const AbstractMatrix<T>& aMatrix);
+
+    /*
+     * Purpose: Get Q matrix
+     * Pre: operator() has been run
+     * Param: None
+     * Post: None
+     * Return: Q matrix
+     */
     DenseMatrix<T> getQ() {return q;}
+
+    /*
+     * Purpose: Get R matrix
+     * Pre: operator() has been run
+     * Param: None
+     * Post: None
+     * Return: R matrix
+     */
     UpperTriangularMatrix<T> getR() {return r;}
   private:
     DenseMatrix<T> q;
