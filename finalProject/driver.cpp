d2c246ce229b03b737f1a7daeedbd3fc92cfcbc6
Cazadorro <snb4y4@mst.edu>
2016-05-06 17:54:00 -0500

    added makefile and also added print formatting functions

--- finalProject/driver.cpp
+++ finalProject/driver.cpp
@@ -6,6 +6,34 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+#define line_width 20
+#define number_max 9
+#define default_precision 5
+using namespace std;
+
+///@brief sends a message out with cout with format so output appears centered
+///and uses '=' for filling
+///@pre none
+///@post message is formatted to terminal
+void print_seperator(string message)
+{
+  cout << setfill('=');
+  cout << setw((line_width + 5) - (message.size() / 2)) << left << "====="
+      << setw(line_width + (message.size() / 2)) << left << message << setw(7)
+      << left << "=====" << endl;
+  cout << setfill(' ');
+}
+
+///@brief sets the format for the given os object
+///@pre none
+/// message is formatted with width set width, precision of precision, and right alignment
+void print_formatter(std::ostream & os, const int precision = default_precision,
+    const int width = number_max)
+{
+  os << setw(width) << fixed << setprecision(precision) << right;
+}
+
+
 double topFunction(double x, double y);
 double bottomFunction(double x, double y);
 double leftFunction(double x, double y);
