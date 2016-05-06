b0b4ea4eed89bfa6aa9caf819997d2ec1e44564a
Jordan Giacone <Jmgiacone@gmail.com>
2016-05-06 17:55:05 -0500

    Added in real f'n values

--- finalProject/driver.cpp
+++ finalProject/driver.cpp
@@ -14,23 +14,23 @@ double gFunction(double x, double y);
 
 int main()
 {
-  const size_t x = 4;
+  const size_t x = 3;
   laplaceBVector<topFunction, bottomFunction, leftFunction, rightFunction, gFunction, double>(x);
   return 0;
 }
 
 double topFunction(double x, double y)
 {
-  return 1.0;
+  return 2 * bottomFunction(x, y);
 }
 double bottomFunction(double x, double y)
 {
-  return 1.0;
+  return 1 - x*x;
 }
 
 double leftFunction(double x, double y)
 {
-  return 1.0;
+  return 1 + y*y;
 }
 
 double rightFunction(double x, double y)
@@ -40,5 +40,5 @@ double rightFunction(double x, double y)
 
 double gFunction(double x, double y)
 {
-  return 1.0;
+  return 0;
 }
